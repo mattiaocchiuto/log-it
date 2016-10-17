9da6b7e5a61cf463785476e2585900360b1dc80c
Mattia Occhiuto <mattiaocchiuto@MacBook-Pro-di-Mattia.local>
2016-10-17 08:38:27 +0200

    Add separate files for catch and log functions

---
+++ src/catcher.js
@@ -0,0 +1,21 @@
+const formatError = error => error;
+
+export function funcExecutor(funcToCall, args = [], scope = undefined) {
+    try {
+        funcToCall.apply(scope, ...args);
+    } catch (e) {
+        // TODO trova modo per fare la compose nativa
+        config.loggingFunction(formatError(e));
+
+        throw e;
+    }
+};
+
+export function attachGlobalHandler() {
+    config.scope.onerror = () => {
+        // TODO trova modo per fare la compose nativa
+        config.loggingFunction(formatError({ arguments }));
+
+        return false;
+    };
+};
