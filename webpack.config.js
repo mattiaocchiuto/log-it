00353f9cfbb1564fc0b81860e4ebe51b6544b01b
Mattia Occhiuto <mattiaocchiuto@MacBook-Pro-di-Mattia.local>
2016-10-17 08:37:11 +0200

    Fix project configuration file, stage-2 include

--- webpack.config.js
+++ webpack.config.js
@@ -1,15 +1,15 @@
-var webpack = require("webpack");
+var webpack = require('webpack');
 
-var libraryName = 'Comprehensions';
+var libraryName = 'LogIt';
 var fileName = 'index';
 
 module.exports = {
-    entry: ["./src/" + fileName + ".js"],
-    devtool: "source-map",
+    entry: ['./src/' + fileName + '.js'],
+    devtool: 'source-map',
     output: {
         path: __dirname,
-        filename: "dist/" + fileName + ".min.js",
-        libraryTarget: "umd",
+        filename: 'dist/' + fileName + '.min.js',
+        libraryTarget: 'umd',
         library: libraryName,
         umdNamedDefine: true
     },
@@ -20,7 +20,7 @@ module.exports = {
                 exclude: /(node_modules|bower_components)/,
                 loader: 'babel',
                 query: {
-                    presets: ['es2015']
+                    presets: ['es2015', 'stage-2'],
                 }
             }
         ]
